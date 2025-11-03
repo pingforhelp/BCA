1213290c86a58dd482c79b78a689ccd17de8a3f5
WeridGuyIdk <tamimtasira@gmail.com>
2025-11-03 17:36:23 +0530

    Create config.js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,30 @@
+export default {
+  title: 'notes.tamims.space',
+  description: 'xx',
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'Communication Skills',
+        collapsed: false,
+        items: [
+          { text: 'Everyday Phrases', link: '/communication skills/Everyday_Phrases' },
+          { text: 'Mouth Yoga', link: '/communication skills/Mouth_Yoga' },
+        ],
+      },
+      {
+        text: 'Computer Science',
+        collapsed: false,
+        items: [
+          { text: 'Computer Fundamentals', link: '/computer sci/Computer_Fundamentals' },
+          { text: 'Unit 1 – C', link: '/computer sci/Unit-1_C' },
+          { text: 'Unit 2 – C', link: '/computer sci/Unit-2C_' },
+        ],
+      },
+      {
+        text: 'English',
+        collapsed: false,
+        items: [{ text: 'Basic Grammar', link: '/english/Basic-Grammar' }],
+      },
+    ],
+  },
+};
