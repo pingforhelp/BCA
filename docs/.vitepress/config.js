0fdbdec4750bcb1de293c5c85d0e3815cf2c4d16
WeridGuyIdk <tamimtasira@gmail.com>
2025-11-04 15:39:57 +0530

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,29 +1,33 @@
 export default {
   title: 'notes.tamims.space',
-  description: 'xx',
+  description: 'BCA Notes and Resources',
   themeConfig: {
     sidebar: [
       {
         text: 'Communication Skills',
         collapsed: false,
         items: [
-          { text: 'Everyday Phrases', link: '/communication skills/Everyday_Phrases' },
-          { text: 'Mouth Yoga', link: '/communication skills/Mouth_Yoga' },
+          { text: 'Everyday Phrases', link: '/communication skills/everyday_phrases' },
+          { text: 'Mouth Yoga', link: '/communication skills/mouth-yoga' },
         ],
       },
       {
         text: 'Computer Science',
         collapsed: false,
         items: [
-          { text: 'Computer Fundamentals', link: '/computer sci/Computer_Fundamentals' },
-          { text: 'Unit 1 – C', link: '/computer sci/Unit-1_C' },
-          { text: 'Unit 2 – C', link: '/computer sci/Unit-2C_' },
+          { text: 'Computer Fundamentals', link: '/computer_sci/computer_fundamentals/computer_fundamentals_unit-1' },
+          { text: 'Office Automation', link: '/computer_sci/computer_fundamentals/office_automation' },
+          { text: 'Unit 1 – C', link: '/computer_sci/c/u-1c' },
+          { text: 'Unit 2 – C', link: '/computer_sci/c/u-2c' },
+          { text: 'Unit 3 – C', link: '/computer_sci/c/u-3c' },
         ],
       },
       {
         text: 'English',
         collapsed: false,
-        items: [{ text: 'Basic Grammar', link: '/english/Basic-Grammar' }],
+        items: [
+          { text: 'Chapter 01', link: '/english/chapter-01' },
+        ],
       },
     ],
   },
