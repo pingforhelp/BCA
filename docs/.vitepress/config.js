91ba0c076b6f808d393dd0053d9a7758839c466d
WeridGuyIdk <tamimtasira@gmail.com>
2025-11-05 11:45:08 +0530

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -18,7 +18,8 @@ export default {
         items: [
           { text: 'Computer Fundamentals', link: '/computer-sci/computer-fundamentals/computer_fundamentals_unit-1' },
           { text: 'Unit 1 – C', link: '/computer-sci/c/u-1c' },
-          { text: 'Unit 2 – C', link: '/computer-sci/c/u-2c' },
+          { text: 'Unit 2 – C', link: '/computer-sci/c/u-2c' }, 
+          { text: 'Unit 3 — C' , link: ' /computer-sci/c/u-2c'},
           
         ],
       },
