1ab7bd9caec9bff1fd43d92a25b0905d3fd03671
WeridGuyIdk <tamimtasira@gmail.com>
2026-02-05 17:46:08 +0530

    Update config.ts

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -106,7 +106,7 @@ export default withMermaid(
         { icon: "github", link: "https://github.com/pingforhelp/BCA" },
         { icon: "linkedin", link: "https://linkedin.com/in/tamimtasira" },
         { icon: "twitter", link: "https://x.com/Tamim_056" },
-        { icon: "e-mail", link: "mailto:bca-github@tamimtasira.in"}
+        { icon: "mail", link: "mailto:bca-github@tamimtasira.in"}
       ],
 
       search: { provider: "local" },
